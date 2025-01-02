fc1874b0e170cb4f67c47043b760e3552cd499f4
endulum <71853916+endulum@users.noreply.github.com>
2025-01-02 13:52:14 -0800

    prevent flyout menu from rendering if it has no children

--- src/components/FlyoutMenu.tsx
+++ src/components/FlyoutMenu.tsx
@@ -1,5 +1,5 @@
 import { MoreHoriz, Close } from "@mui/icons-material";
-import { useRef } from "react";
+import { useRef, Children } from "react";
 import { useBoolean } from "usehooks-ts";
 import { useOnClickOutside } from "usehooks-ts";
 
@@ -10,7 +10,7 @@ export function FlyoutMenu({
 }: {
   x: "left" | "right";
   y: "bottom" | "top";
-  children: Array<JSX.Element | boolean | undefined | null | "">; // jeez
+  children: React.ReactNode;
 }) {
   const { value: opened, setTrue: open, setFalse: close } = useBoolean(false);
   const menu = useRef<HTMLDivElement>(null);
@@ -18,30 +18,31 @@ export function FlyoutMenu({
     if (opened) close();
   });
 
-  return (
-    <div className="flyout" onClick={open}>
-      <button
-        className="button plain flyout-button"
-        aria-haspopup="menu"
-        aria-expanded={opened ? "true" : "false"}
-      >
-        {opened ? <Close /> : <MoreHoriz />}
-      </button>
-      {opened && (
-        <div
-          ref={menu}
-          role="menu"
-          className="menu flex-col align-start gap-0-25"
-          style={{
-            ...(x === "right" && { left: 0 }),
-            ...(x === "left" && { right: 0 }),
-            ...(y === "bottom" && { top: "2rem" }),
-            ...(y === "top" && { bottom: "2rem" }),
-          }}
+  if (Children.toArray(children).length > 0)
+    return (
+      <div className="flyout" onClick={open}>
+        <button
+          className="button plain flyout-button"
+          aria-haspopup="menu"
+          aria-expanded={opened ? "true" : "false"}
         >
-          {children}
-        </div>
-      )}
-    </div>
-  );
+          {opened ? <Close /> : <MoreHoriz />}
+        </button>
+        {opened && (
+          <div
+            ref={menu}
+            role="menu"
+            className="menu flex-col align-start gap-0-25"
+            style={{
+              ...(x === "right" && { left: 0 }),
+              ...(x === "left" && { right: 0 }),
+              ...(y === "bottom" && { top: "2rem" }),
+              ...(y === "top" && { bottom: "2rem" }),
+            }}
+          >
+            {children}
+          </div>
+        )}
+      </div>
+    );
 }
