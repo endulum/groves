28d537511cd9b95b0ec7910d36c33abdd381a109
endulum <71853916+endulum@users.noreply.github.com>
2025-01-08 23:55:51 -0800

    trim down About page text, give some spacing to `ul`s

--- src/components/routes/AboutRoute.tsx
+++ src/components/routes/AboutRoute.tsx
@@ -7,10 +7,14 @@ export function AboutRoute() {
     <>
       <h2>About Groves</h2>
       <p>
-        Groves is a semiclone of Reddit with an arboreal flavor. Communities are
-        likened to <IconSpan icon={<Forest />} text="groves" />, with posts
-        among them being <IconSpan icon={<Park />} text="trees" />, and post
-        comments being <IconSpan icon={<Spa />} text="leaves" /> of a tree.
+        Groves is a semiclone of{" "}
+        <a href="https://redditinc.com/" target="_blank">
+          Reddit
+        </a>{" "}
+        with an arboreal flavor. Communities are likened to{" "}
+        <IconSpan icon={<Forest />} text="groves" />, with posts among them
+        being <IconSpan icon={<Park />} text="trees" />, and post comments being{" "}
+        <IconSpan icon={<Spa />} text="leaves" /> of a tree.
       </p>
 
       <h3 className="mt-1 mb-0-5">"Semiclone"?</h3>
@@ -24,7 +28,9 @@ export function AboutRoute() {
           />
           . In lists, such as a community's post search, verdancy is used to
           rank content, using roughly the same scoring types that Reddit uses
-          ('Top', 'Hot', 'Controversial', etc.).
+          ('Top', 'Hot', 'Controversial', etc.). Users accumulate invidividual
+          verdancy scores, visible on their profile, dependent on the votes
+          their content gather.
         </li>
         <li>
           Communities can be moderated, and customized by moderation. Moderation
@@ -45,89 +51,22 @@ export function AboutRoute() {
           </ul>
         </li>
         <li>
-          Communities can be followed, and content from followed communities is
-          pooled into a personal feed.
+          Communities can be followed, and posts from followed communities are
+          pooled into a personal feed. The feed orders posts from latest posted.
+        </li>
+        <li>
+          Wikis, posts, and replies use{" "}
+          <a href="https://commonmark.org/help/">limited markdown.</a> Supported
+          markdown formats are:
+          <ul>
+            <li>First- to third-level headings</li>
+            <li>Basic formatting: bold, italic, strikethrough</li>
+            <li>Links</li>
+            <li>Images</li>
+            <li>Lists, both ordered and unordered</li>
+          </ul>
         </li>
       </ul>
-
-      <h2>Navigating Groves</h2>
-      <h3 className="mb-1">Finding content</h3>
-      <p>
-        You can most easily find and filter communities relevant to your
-        interests through the <b>explore</b> search.
-      </p>
-      <p>
-        You can <b>follow</b> a community so that its content appears on your
-        personal feed.
-      </p>
-      <p>
-        Your feed consists of posts from all communities you follow, ordered by
-        latest date of posting.
-      </p>
-      <h3 className="mt-1 mb-1">Creating content</h3>
-      <p>
-        If a community is not marked as frozen, anyone is free to create a post
-        under the community.
-      </p>
-      <p>
-        Likewise, if a post is not marked as frozen, anyone is free to create a
-        reply under the post.
-      </p>
-      <p>
-        Posts, replies, and wikis use{" "}
-        <a href="https://commonmark.org/help/">limited markdown.</a> Supported
-        markdown formats are:
-      </p>
-      <ul className="mt-0-5 mb-0-5">
-        <li>First- to third-level headings</li>
-        <li>Basic formatting: bold, italic, strikethrough</li>
-        <li>Links</li>
-        <li>Images</li>
-        <li>Lists, both ordered and unordered</li>
-      </ul>
-      <h3 className="mt-1 mb-1">Voting on content</h3>
-      <p>
-        Users accumulate verdancy from votes on their content by other users.
-        Giving a reply or post a vote is a way to show appreciation - or the
-        opposite - for the content. Votes also help more relevant content appear
-        more or less prominently in ranked search.
-      </p>
-      <p>
-        If a post is not marked as frozen, and its root community is not marked
-        as frozen, voting can be done freely on it and its replies.
-      </p>
-      <p>
-        You can see a user's accumulated verdancy on their profile. Content with
-        negative total verdancy scores do not negate the user's total verdancy.
-        This way, user verdancy is always rewardingly positive, and scales up
-        with how often the user engages with Groves.
-      </p>
-      <h3 className="mt-1 mb-1">Managing a community</h3>
-      <p>
-        Creating a community automatically grants the creator admin privileges
-        over the community. All details provided in community creation can be
-        changed later by the admin, so there's no worry to get it right the
-        first time.
-      </p>
-      <p>
-        The <b>moderation</b> tab of a community consists of the current admin
-        as well as a roster of current moderators. The admin can promote and
-        demote moderators, adjusting the mod team as they see fit, and even pass
-        admin privileges to another user. The admin can also choose to freeze
-        the entire community, functionally "archiving" it.
-      </p>
-      <p>
-        The <b>wiki</b> of a community is a document in markdown, a place for a
-        large amount of written information for readers, such as community
-        rules, or an FAQ pertaining to the community subject. General moderation
-        can edit this wiki.
-      </p>
-      <p>
-        In post view, the admin is indicated in gold with a starred shield.
-        Moderators are indicated in blue with a plain shield. Moderators can
-        manage a post by freezing it, ceasing all voting and replying activity,
-        or hiding any of its replies, ceasing voting activity on it.
-      </p>
     </>
   );
 }
